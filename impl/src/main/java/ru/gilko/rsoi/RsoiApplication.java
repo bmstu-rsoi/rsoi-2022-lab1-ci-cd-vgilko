88a38ee5198f2cabf824cd1baed334ba745f3797
vgilko <v.gilko@mkskom.com>
2022-10-01 13:45:31 +0300

    conf: Создать класс приложения
    * Создан класс приложения
    * Добавлены настройки приложения

---
+++ impl/src/main/java/ru/gilko/rsoi/RsoiApplication.java
@@ -0,0 +1,13 @@
+package ru.gilko.rsoi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RsoiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RsoiApplication.class, args);
+    }
+
+}
